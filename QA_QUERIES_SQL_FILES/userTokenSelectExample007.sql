cda72eb90c524e6bc209833b63620cac2a477049
birish87 <45980316+birish87@users.noreply.github.com>
2023-10-11 13:36:34 -0400

    Update userTokenSelectExample007.sql

--- QA_QUERIES_SQL_FILES/userTokenSelectExample007.sql
+++ QA_QUERIES_SQL_FILES/userTokenSelectExample007.sql
@@ -1,5 +1,5 @@
 INSERT INTO public."QA_QUERIES_NEW" (name, query)
-VALUES ('userTokenSelectExample002300', 'Delete from reporting.rpt_bsc_claim_history;
+VALUES ('userTokenSelectExample002300', $$Delete from reporting.rpt_bsc_claim_history;
 Delete from reporting.rpt_bsc_dsr_claim_gpi_outcome_prep_data;
 
 INSERT INTO reporting.rpt_bsc_dsr_claim_gpi_outcome_prep_data
@@ -24,4 +24,4 @@ VALUES(current_timestamp, 'R-1UBNS-2HCWW', '004336', '77993333', '2022-10-15 15:
 
 INSERT INTO reporting.rpt_bsc_claim_history
 (id, patient_identifier, new_patient_identifier, carrier_id, account_id, group_id, adjudication_datetime, date_of_service, prescription_service_reference_number, service_provider_id, service_provider_id_qualifier, med_ndc, med_name, med_brand, med_msc, specific_product_id, concept_name, quantity_dispensed, days_supply, fill_number, gpi_14, patient_cost, total_cost, inserted_at, transaction_code, "row_number", med_type)
-VALUES(16, '00433677993333910217343001970-01-01', 'bsc910217343001970-01-01', 'ChosenMed', NULL, 'E00010022000CMCM0002', date_trunc('month', current_date - interval '1' month) + interval '19 DAY', date_trunc('month', current_date - interval '1' month) + interval '19 DAY', '000012285355', '1578544474', '01', '70436001206', 'Desvenlafaxine 50mg ER Tablet', 'N', 'Y', 15382, 'estrogens, excluding hormonal contraceptives', 30.0, 90, '01', '58180020207520', 5.0, 52.22, date_trunc('month', current_date - interval '1' month) + interval '19 DAY', '1', 1, 'M');');
+VALUES(16, '00433677993333910217343001970-01-01', 'bsc910217343001970-01-01', 'ChosenMed', NULL, 'E00010022000CMCM0002', date_trunc('month', current_date - interval '1' month) + interval '19 DAY', date_trunc('month', current_date - interval '1' month) + interval '19 DAY', '000012285355', '1578544474', '01', '70436001206', 'Desvenlafaxine 50mg ER Tablet', 'N', 'Y', 15382, 'estrogens, excluding hormonal contraceptives', 30.0, 90, '01', '58180020207520', 5.0, 52.22, date_trunc('month', current_date - interval '1' month) + interval '19 DAY', '1', 1, 'M');$$);
