80c384842607714e75651dc39bad0d188b33d566
birish87 <45980316+birish87@users.noreply.github.com>
2023-10-18 17:24:20 -0400

    Update demo.sql

--- QA_QUERIES_SQL_FILES/demo.sql
+++ QA_QUERIES_SQL_FILES/demo.sql
@@ -1,4 +1,4 @@
-INSERT INTO public."QA_QUERIES_NEW" (name, query)
+INSERT INTO public."QA_QUERIES" (name, query)
 VALUES ('demoSQL', $$Delete from reporting.rpt_bsc_claim_history;
 Delete from reporting.rpt_bsc_dsr_claim_gpi_outcome_prep_data;
 
